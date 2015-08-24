10f474554469102116ab3caef5a39c7f00462f1d
galarchenko <cristophergalarce@gmail.com>
2015-08-24 01:55:39 -0300

    Todo el primer repo
    Subo todo el repo del landing de la primera etapa de mi portfolio.
    Enlazado gh-pages con mi hosting y el custom domain elalquimista.pro :)

---
+++ php/enviarcorreo.php
@@ -0,0 +1,27 @@
+<?php
+$para = 'cristophergalarce@gmail.com';
+$asunto = 'Contacto desde elalquimista.pro';
+
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$mensaje = $_POST['mensaje'];
+
+$msjCorreo = "Nombre: $nombre\n\nE-Mail: $email\n\nMensaje:\n$mensaje";
+
+$cabecera = 'From: '.$nombre.' <'.$email.'>'."\r\n".
+			'Reply-To: '.$email."\r\n";
+
+if ($_POST['submit']){
+if (@mail($para, $asunto, $msjCorreo, $cabecera)){
+echo "<script language='javascript'>
+alert('Correo enviado :) Dame un dia para responderte.');
+window.location.href = 'http://elalquimista.pro';
+</script>";
+} else {
+echo "<script language='javascript'>
+alert('Fallo el envio :( Refresca la pagina y reintenta.');
+window.location.href = 'http://elalquimista.pro';
+</script>";
+}
+}
+?>
