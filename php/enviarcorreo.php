aa4060ac3c12220a618e59f0ffaa859c2f575245
galarchenko <cristoph.galarchenko@gmail.com>
2016-11-09 11:23:24 -0300

    revision 1.1 .php

--- php/enviarcorreo.php
+++ php/enviarcorreo.php
@@ -12,15 +12,15 @@ $cabecera = 'From: '.$nombre.' <'.$email.'>'."\r\n".
 			'Reply-To: '.$email."\r\n";
 
 if ($_POST['submit']){
-if (@mail($para, $asunto, $msjCorreo, $cabecera)){
-echo "<script language='javascript'>
-alert('Correo enviado :) Dame un dia para responderte.');
-window.location.href = '../';
-</script>";
-} else {
-echo "<script language='javascript'>
-alert('Fallo el envio :( Refresca la pagina y reintenta.');
-window.location.href = '../';
-</script>";
-}
+	if (@mail($para, $asunto, $msjCorreo, $cabecera)){
+		echo "<script language='javascript'>
+		alert('Correo enviado :) Dame un dia para responderte.');
+		window.location.href = '../';
+		</script>";
+	} else {
+		echo "<script language='javascript'>
+		alert('Fallo el envio :( Refresca la pagina y reintenta.');
+		window.location.href = '../';
+		</script>";
+	}
 }
